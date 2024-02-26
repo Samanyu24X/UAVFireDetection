ae4e428c0c766223be18bb71c0c867dd2eadbde6
Samanyu <35706393+Samanyu24X@users.noreply.github.com>
2024-02-25 21:44:16 -0800

    moved things around and added some interfacing

--- drone_code.py
+++ drone_code.py
@@ -7,37 +7,40 @@ import datetime
 from xbee import XBee
 from digi.xbee.devices import XBeeDevice
 import serial
+
 print(serial.__file__)
 
-PORT = "/dev/ttyUSB0"
-BAUDRATE = 9600
-#open serial port
-device = XBeeDevice(PORT, BAUDRATE)
-device.open()
-print("The xbee is open")
-lat = float(34.04285)
-lon = float(-117.81194)
-Targ_Location = dk.LocationGlobalRelative(lat, lon,30)  
-print ("Coordinates saved, Connecting to vehicle")
-connection_string = '/dev/ttyACM0'
-vehicle = dk.connect(connection_string, wait_ready=True, baud=115200)
-print("connection is secure")
 
 shortDate = datetime.datetime.today().strftime('%Y_%m_%d')
-outputFile = "coordinate_output_" + shortDate +".txt"
+outputFile = "coordinate_output_" + shortDate +".txt" # initializing an output file for logs
 f = open(outputFile, "a")
 
+''' Prompts user to pick flight mode'''
+def get_flight_mode():
+    print("Select flight mode:\n[1] - Autonomous\n[2] - Manual\n")
+    mode = input()
+    while mode != '1' and mode != '2':
+        print("Invalid input. Try again")
+        print("Select flight mode:\n[1] - Autonomous\n[2] - Manual\n")
+        mode = input()
+    if mode == '1':
+        return 1
+    elif mode =='2':
+        return 2
+
+
 # working auto from past program
+''' Autonomous takeoff function from previous code'''
 def arm_and_takeoff(aTargetAltitude):
     print("Basic pre-arm checks")
-    while not vehicle.is_armable:
+    while not pixhawkVehicle.is_armable:
         print(" Waiting for vehicle to initialise...")
         time.sleep(1)
 
     print("Arming motors")
-    vehicle.mode = dk.VehicleMode("GUIDED")
-    vehicle.armed = True
-    while not vehicle.armed:
+    pixhawkVehicle.mode = dk.VehicleMode("GUIDED")
+    pixhawkVehicle.armed = True
+    while not pixhawkVehicle.armed:
         print(" Waiting for arming...")
         time.sleep(1)
 
@@ -46,15 +49,16 @@ def arm_and_takeoff(aTargetAltitude):
     aTargetAltitude = 10
 
     print("Taking off!")
-    vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
+    pixhawkVehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
     while True:
-        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
-        if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
+        print(" Altitude: ", pixhawkVehicle.location.global_relative_frame.alt)
+        if pixhawkVehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
             print("Reached target altitude")
             break
         time.sleep(1)
 
 
+''' Sends information to Xbee'''
 def send_UAV_data_Xbee(ICAO, pos_lat, pos_lon, pos_alt_rel,velocity,airspeed,battery):
     #print("In send ADSB funtion\n")
     msg = "ICAO: " + ICAO + '\n'
@@ -68,16 +72,18 @@ def send_UAV_data_Xbee(ICAO, pos_lat, pos_lon, pos_alt_rel,velocity,airspeed,bat
     print(msg)
     return msg
 
+''' Takes in location data and attempts to go there'''
 def goToLocation(latPosition, lonPosition, altPosition):
     location = dk.LocationGlobal(latPosition, lonPosition, altPosition)
-    vehicle.simple_goto(location)
+    pixhawkVehicle.simple_goto(location)
     return 1
 
+''' Takes in change in altitude and makes adjustment'''
 def changeAltitude(change):
-    location = dk.LocationGlobal(vehicle.location.global_relative_frame.lat, 
-                              vehicle.location.global_relative_frame.lon, 
-                              vehicle.location.global_relative_frame.alt + change)
-    vehicle.simple_goto(location)
+    location = dk.LocationGlobal(pixhawkVehicle.location.global_relative_frame.lat, 
+                              pixhawkVehicle.location.global_relative_frame.lon, 
+                              pixhawkVehicle.location.global_relative_frame.alt + change)
+    pixhawkVehicle.simple_goto(location)
     return 1
 
 # the data that we get here is used to command the drone to do someting
@@ -114,22 +120,51 @@ def callbackFunction(XBeeMessage):
         goToLocation(int(stringData[1:11]), int(stringData[12:23]), int(stringData[24:len(stringData)]))
     return 1
 
-device.add_data_received_callback(callbackFunction())
+def detect_while_spanning_area():
+    return
+
+
+''' This section takes care of connections (xbee, pixhawk, etc) '''
+
+PORT = "/dev/ttyUSB0" # port for xbee
+BAUDRATE = 9600 # BAUDRATE for xbee
+#open serial port
+xbeeDevice = XBeeDevice(PORT, BAUDRATE) # connect the device
+xbeeDevice.open()
+print("The xbee is open")
+
+lat = float(34.04285)
+lon = float(-117.81194)
+Targ_Location = dk.LocationGlobalRelative(lat, lon,30)
+print ("Coordinates saved, Connecting to vehicle")
+connection_string = '/dev/ttyACM0' # connection string for pixhawk
+pixhawkVehicle = dk.connect(connection_string, wait_ready=True, baud=115200) # connecting to pixhawk
+print("connection is secure")
+
+''' End of initialization section '''
+
+
+xbeeDevice.add_data_received_callback(callbackFunction()) # add the callback function
+
+detect_while_spanning_area()
+
+
+
 while 1:
-    current_loc = [vehicle.location.global_relative_frame.lat, vehicle.location.global_relative_frame.lon] #current coordinates of the drone
+    current_loc = [pixhawkVehicle.location.global_relative_frame.lat, pixhawkVehicle.location.global_relative_frame.lon] #current coordinates of the drone
     print("sending data...")
     print(current_loc)
-    device.send_data_broadcast(send_UAV_data_Xbee(
+    xbeeDevice.send_data_broadcast(send_UAV_data_Xbee(
         "A", 
-        vehicle.location.global_relative_frame.lat, 
-        vehicle.location.global_relative_frame.lon, 
-        vehicle.location.global_relative_frame.alt,
-        vehicle.velocity,
-        vehicle.airspeed, 
-        vehicle.battery.voltage))
+        pixhawkVehicle.location.global_relative_frame.lat, 
+        pixhawkVehicle.location.global_relative_frame.lon, 
+        pixhawkVehicle.location.global_relative_frame.alt,
+        pixhawkVehicle.velocity,
+        pixhawkVehicle.airspeed, 
+        pixhawkVehicle.battery.voltage))
     
     f.write(str(int(time.time()*1000))+' ')
-    f.write(str(vehicle.location.global_relative_frame.lat)+ ' ')
-    f.write(str(vehicle.location.global_relative_frame.lon)+ '\n')
+    f.write(str(pixhawkVehicle.location.global_relative_frame.lat)+ ' ')
+    f.write(str(pixhawkVehicle.location.global_relative_frame.lon)+ '\n')
     time.sleep(0.2)
 
